c4c75e00a94cce15e6ef50cefe9bea86f5012fc6
Chanuka Sandeepa <chanursandeepa@gmail.com>
2020-12-05 14:56:32 +0530

    chaging

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const Experience = require('./models/Experience')
+const multer = require('multer')
+
+const app = express()
+
+const PORT = process.env.PORT || 8000
+const upload = multer({ dest : '/uploads' })
+const URL = 'mongodb+srv://user:12345@cluster0.7pxt3.mongodb.net/imessage?retryWrites=true&w=majority'
+
+mongoose.connect(URL,{
+    useUnifiedTopology : true,
+    useNewUrlParser : true
+    
+})
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Done')
+})
+
+app.post('/create', async (req, res) => {
+    console.log(req.body)
+    const exp = new Experience(req.body)
+    const sv = await exp.save()
+    console.log(sv)
+    res.send({ status  : 'success' })
+})
+
+app.post('/upload', upload.array('images'), async (req, res) => {
+    console.log(req.body)
+    res.send({ status  : 'success' })
+})
+
+app.get('/get', async (req, res) => {
+    console.log('called')
+    const result = await Experience.find({})
+    res.send(result)
+})
+
+app.get('/get/:id', async (req, res) => {
+    console.log(req.params.id)
+    const result = await Experience.findById(req.params.id)
+    res.send(result)
+})
+
+app.listen(PORT, () => {
+    console.log('server is listening ' + PORT + ' ')
+})
